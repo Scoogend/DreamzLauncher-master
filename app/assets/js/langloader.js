a844914e5b9a6f4483c2859eecda0c372ce8b6bc
Scoogend <girot.hugo@yahoo.com>
2024-09-22 16:22:43 +0100

    launcher-terminé

--- app/assets/js/langloader.js
+++ app/assets/js/langloader.js
@@ -5,14 +5,23 @@ const merge = require('lodash.merge')
 
 let lang
 
-exports.loadLanguage = function(id){
-    lang = merge(lang || {}, toml.parse(fs.readFileSync(path.join(__dirname, '..', 'lang', `${id}.toml`))) || {})
+exports.loadLanguage = function(id) {
+    try {
+        const languageFile = fs.readFileSync(path.join(__dirname, '..', 'lang', `${id}.toml`), 'utf-8')
+        lang = merge(lang || {}, toml.parse(languageFile) || {})
+    } catch (err) {
+        console.error(`Erreur lors du chargement du fichier de langue ${id}:`, err)
+    }
 }
 
-exports.query = function(id, placeHolders){
+exports.query = function(id, placeHolders) {
     let query = id.split('.')
     let res = lang
-    for(let q of query){
+    for (let q of query) {
+        if (res[q] === undefined) {
+            console.error(`La clé "${id}" est introuvable dans le fichier de langue.`)
+            return '' // Retourner une chaîne vide si la clé n'existe pas
+        }
         res = res[q]
     }
     let text = res === lang ? '' : res
@@ -24,20 +33,20 @@ exports.query = function(id, placeHolders){
     return text
 }
 
-exports.queryJS = function(id, placeHolders){
+exports.queryJS = function(id, placeHolders) {
     return exports.query(`js.${id}`, placeHolders)
 }
 
-exports.queryEJS = function(id, placeHolders){
+exports.queryEJS = function(id, placeHolders) {
     return exports.query(`ejs.${id}`, placeHolders)
 }
 
-exports.setupLanguage = function(){
-    // Load Language Files
+exports.setupLanguage = function() {
+    // Charger les fichiers de langue
     exports.loadLanguage('en_US')
-    // Uncomment this when translations are ready
+    // Décommentez cette ligne lorsque les traductions seront prêtes
     //exports.loadLanguage('xx_XX')
 
-    // Load Custom Language File for Launcher Customizer
+    // Charger le fichier de langue personnalisé pour le customizer du launcher
     exports.loadLanguage('_custom')
 }
